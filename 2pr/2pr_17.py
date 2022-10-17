51b87df67eb5f5885362f4f5783b434116e1feb2
Dmitry Kartashov <57914020+loneymist@users.noreply.github.com>
2022-10-17 21:39:59 +0700

    b.0.0.1

--- 2pr/2pr_17.py
+++ 2pr/2pr_17.py
@@ -1,6 +1,19 @@
 datamass = []
-with open(r'data.txt','r') as data:
+invalid = []
+i = 0
+j = 0
+c = 0
+with open(r'2pr/data.txt','r') as data:
      lines = data.readlines()
      for line in lines:
         datamass.append(line.split(' '))
-print(datamass)
+datamass.pop(0)
+print(len(lines))
+while i < 4:
+        if len(datamass[i]) == 4:
+                 c += 1
+        else:
+                invalid.append(datamass[i])
+        i += 1         
+print(c)
+print(invalid)
